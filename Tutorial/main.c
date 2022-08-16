600891ff8646ea4099c68de743ef367c04ee0df4
codeReaper0 <boluwatifetella@gmail.com>
2022-08-16 14:29:31 +0100

    C in progress

--- Tutorial/main.c
+++ Tutorial/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
+    char characterName[] = "John";
+    int characterAge = 35;
     printf("Hello world!\n");
+    printf("There once was a man named %s \n", characterName);
+    printf("He was %d years old \n" characterAge);
+    printf("How are you doing? \n");
+
     return 0;
 }
+;
