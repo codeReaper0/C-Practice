ec7265d572caabf1ebb7c037062263d7092bbecc
codeReaper0 <boluwatifetella@gmail.com>
2022-08-18 16:49:11 +0100

    code adjusted

--- Tutorial/main.c
+++ Tutorial/main.c
@@ -7,7 +7,7 @@ int main()
     int characterAge = 35;
     printf("Hello world!\n");
     printf("There once was a man named %s \n", characterName);
-    printf("He was %d years old \n" characterAge);
+    printf("He was %d years old \n", characterAge);
     printf("How are you doing? \n");
 
     return 0;
